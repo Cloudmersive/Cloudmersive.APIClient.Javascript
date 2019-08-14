4777d18504bf9c81bda535d541ed7befd9e2f762
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-08-13 18:28:00 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,1 +1,6 @@
 & wget https://wzrd.in/standalone/cloudmersive-convert-api-client@latest -O ./cloudmersive-document-convert.js
+& wget https://wzrd.in/standalone/cloudmersive-barcodeapi-client@latest -O ./cloudmersive-barcode.js
+& wget https://wzrd.in/standalone/cloudmersive-validate-api-client@latest -O ./cloudmersive-validate.js
+& wget https://wzrd.in/standalone/cloudmersive-image-api-client@latest -O ./cloudmersive-image.js
+& wget https://wzrd.in/standalone/cloudmersive-nlp-api-client@latest -O ./cloudmersive-nlp.js
+& wget https://wzrd.in/standalone/cloudmersive-ocr-api-client@latest -O ./cloudmersive-ocr.js
