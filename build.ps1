54421736b9ceea7baaabc38b96582bd0dacef9a8
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-08-13 18:28:46 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
-& wget https://wzrd.in/standalone/cloudmersive-convert-api-client@latest -O ./cloudmersive-document-convert.js
-& wget https://wzrd.in/standalone/cloudmersive-barcodeapi-client@latest -O ./cloudmersive-barcode.js
-& wget https://wzrd.in/standalone/cloudmersive-validate-api-client@latest -O ./cloudmersive-validate.js
-& wget https://wzrd.in/standalone/cloudmersive-image-api-client@latest -O ./cloudmersive-image.js
-& wget https://wzrd.in/standalone/cloudmersive-nlp-api-client@latest -O ./cloudmersive-nlp.js
-& wget https://wzrd.in/standalone/cloudmersive-ocr-api-client@latest -O ./cloudmersive-ocr.js
+& wget https://wzrd.in/standalone/cloudmersive-convert-api-client@latest -O ./cloudmersive-document-convert-client.js
+& wget https://wzrd.in/standalone/cloudmersive-barcodeapi-client@latest -O ./cloudmersive-barcode-client.js
+& wget https://wzrd.in/standalone/cloudmersive-validate-api-client@latest -O ./cloudmersive-validate-client.js
+& wget https://wzrd.in/standalone/cloudmersive-image-api-client@latest -O ./cloudmersive-image-client.js
+& wget https://wzrd.in/standalone/cloudmersive-nlp-api-client@latest -O ./cloudmersive-nlp-client.js
+& wget https://wzrd.in/standalone/cloudmersive-ocr-api-client@latest -O ./cloudmersive-ocr-client.js
+& wget https://wzrd.in/standalone/cloudmersive-virus-api-client@latest -O ./cloudmersive-virus-client.js
